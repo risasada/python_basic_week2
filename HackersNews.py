e6699c0c62ca62e830b5246237452873ffaecdab
risasada <enki546@gmail.com>
2022-02-02 12:48:04 +0900

    saigo

--- HackersNews.py
+++ HackersNews.py
@@ -6,13 +6,12 @@ def main():
     url = 'https://news.ycombinator.com/'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    links = soup.find_all(class_='title')
-    for link in links:
-            try:
-                print(f"title: {link.contents[0]}")
-                print(f"link: {link.attrs['href']}")
-            except KeyError:
-                pass
+    titles = soup.find_all(attrs={ 'class': 'titlelink' })
+    for title in titles:
+        try:
+            print(f"    title: {title.contents[0]}    link: {title.attrs['href']}")
+        except KeyError:
+            pass
 
 
 if __name__ == '__main__':
